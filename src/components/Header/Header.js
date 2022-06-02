c768ccf502ecb93e46a756278400b8104bf8d60b
grahamguthrie99 <grahamguthrie99@gmail.com>
2022-06-01 21:23:41 -0400

    Added navigation and 404 pages

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,149 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import { Link as RouterLink } from 'react-router-dom';
 import { AuthContext } from "../../session/AuthContext";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import AuthButton from "./AuthButton";
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import LogoutIcon from '@mui/icons-material/Logout';
 
-
+const pages = [
+    { name: 'Start a Game', ref: "/game"},
+    { name :  'Rules', ref: "/rules"}, 
+    { name: "Add a Question", ref : "/question" },
+    { name: 'View All Questions', ref : "/questions" },
+    { name: 'Home', ref : "/home" }
+];
 
 export const Header = () => {
-    const { authState, actions } = useContext(AuthContext);
 
-    function onSubmit() {
-          actions.logout();
+    const { authState, actions } = useContext(AuthContext);
+    const [anchorElNav, setAnchorElNav] = useState(null);
+
+    function onLogout() {
+            actions.logout();
     }
 
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
     return (
-        <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Name TBD
-            </Typography>
-            {authState.user &&    
-                <AuthButton onClick={onSubmit}>Sign Out</AuthButton>
-            } 
-          </Toolbar>
-        </AppBar>
-      </Box>
-       
+        <>
+        {authState.user && <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                <Typography
+                    variant="h6"
+                    noWrap
+                    component="a"
+                    href="/"
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'none', md: 'flex' },
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Deeper.
+                </Typography>
+
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                    >
+                    <MenuIcon />
+                    </IconButton>
+                    <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                        display: { xs: 'block', md: 'none' },
+                    }}
+                    >
+                    {pages.map((page) => (
+                        <MenuItem key={page.name} component={RouterLink} to={page.ref} onClick={handleCloseNavMenu}>
+                            <Typography textAlign="center">{page.name}</Typography>
+                        </MenuItem>
+                    ))}
+                    </Menu>
+                </Box>
+                <Typography
+                    variant="h5"
+                    noWrap
+                    component="a"
+                    href=""
+                    sx={{
+                    mr: 2,
+                    display: { xs: 'flex', md: 'none' },
+                    flexGrow: 1,
+                    fontFamily: 'monospace',
+                    fontWeight: 700,
+                    letterSpacing: '.3rem',
+                    color: 'inherit',
+                    textDecoration: 'none',
+                    }}
+                >
+                    Deeper.
+                </Typography>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                    {pages.map((page) => (
+                    <Button
+                        key={page.name}
+                        onClick={handleCloseNavMenu}
+                        component={RouterLink} 
+                        to={page.ref}
+                        sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                        {page.name}
+                    </Button>
+                    ))}
+                </Box>
+
+                <Box sx={{ flexGrow: 0 }}>
+                    <Tooltip title="Logout">
+                    <IconButton onClick={onLogout} sx={{ p: 0 }}>
+                        <LogoutIcon sx={{ color:  'white'}}/>
+                    </IconButton>
+                    </Tooltip>
+                </Box>
+                </Toolbar>
+            </Container>
+            </AppBar>}
+        </>
     );
 };
+
+export default Header;
+ 
